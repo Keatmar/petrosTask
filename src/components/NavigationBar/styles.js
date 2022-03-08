a4171c421d1434f3d7a2a0a4da72eec3508d5baf
katsosM <keatmar@gmail.com>
2022-03-09 00:56:39 +0200

    empty style

--- src/components/NavigationBar/styles.js
+++ src/components/NavigationBar/styles.js
@@ -2,22 +2,10 @@ import { makeStyles } from '@mui/styles';
 import { height, margin, padding } from '@mui/system';
 export default makeStyles(
   (theme) => ({
-    root: {
-      width: '98%',
-      backgroundColor: '#FFFFFF',
-      position: 'fixed',
-      height: 40,
-      padding: '1%',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
+    root: {},
 
     rightElements: {
-      '& img': {
-        marginRight: '10px',
-        cursor: 'pointer'
-      }
+      '& img': {}
     }
   }),
   { name: 'MainBar' }
