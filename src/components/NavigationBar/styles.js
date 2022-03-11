e18be121946ce6918e9a12275b1b5da3db15347f
petros <petrosworks@gmail.com>
2022-03-11 17:40:07 +0200

    check

--- src/components/NavigationBar/styles.js
+++ src/components/NavigationBar/styles.js
@@ -2,10 +2,22 @@ import { makeStyles } from '@mui/styles';
 import { height, margin, padding } from '@mui/system';
 export default makeStyles(
   (theme) => ({
-    root: {},
+    root: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      height: '55px',
+      backgroundColor: 'white',
+      boxShadow: '0px 10px 24px #D6D6D6',
+      padding: '0px 15px'
+    },
 
     rightElements: {
-      '& img': {}
+      '& img': {
+       
+     
+     
+      }
     }
   }),
   { name: 'MainBar' }
